3eef924171638be128baecc39ffbe2d09c56d4a6
rafeee.ir <rafeee.ir@gmail.com>
2021-06-07 00:22:22 +0430

    Final
    لاراول 8 + بوت استرپ راستچین  5 + فونت وزیر
    با استفاده از لاراول میکس

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <title>لا</title>
+  </head>
+
+<body>
+    <div class="container pt-5">
+    <h1>لاراول نسخه 8 + بوت استرپ نسخه 5</h1>
+        <div class="alert alert-success mt-5" role="alert">
+            این صفحه با بوت استرپ RTL در بستر لاراول آماده سازی شده است
+        </div>    
+    </div>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
+
+
