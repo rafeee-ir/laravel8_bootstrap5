259a38e505fcb8f545f923a5cae72736b3bd55d7
hermessharif <hermessharif1400@gmail.com>
2021-07-18 14:07:21 +0430

    API Tested
    Api

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,15 +14,39 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     public function connectToApi(){
         $client = new Client(array(
+            'base_uri' => 'https://mdapp.kins.ir/cins/',
             'cookies' => true,
+            'timeout' => 2.0,
         ));
-        $client->request('POST', 'https://mdapp.kins.ir/cins/login',
+        $client->request('POST', 'login',
             [    'form_params' => [
-                'username' => 'e.merkori',
-                'password' => 'QWErty!@#456',
+                'username' => env('MDAPP_USERNAME',null),
+                'password' => env('MDAPP_PASSWORD',null),
                 'grant_type' => 'password',
             ]
             ]);
-        return $client;
+            return $client;
     }
+    public function apiGet($uri){
+        $res = $this->connectToApi()->request('GET',$uri,[
+            'accept' => 'application/json;charset=UTF-8'
+        ]);
+        return json_decode($res->getBody()->getContents(),true);
+
+//        @foreach($array_data as $key=>$value){
+//            <tr>
+//    <td>{{$value["id"]}}</td>
+//    <td>{{$value["user_id"]}}</td>
+//    <td>{{$value["patient_name"]}}</td>
+//    <td>{{$value["age"]}}</td>
+//    <td>{{$value["sex"]}}</td>
+//</tr>
+//            @endforeach
+
+    }
+    public function apiPost($uri){
+        $res = $this->connectToApi()->get($uri)->getBody()->getContents();
+        return json_decode($res,true);
+    }
+
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Cookie\CookieJar;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Http;
-use App\Http\Controllers\Controller;
 
 
 class HomeController extends Controller
@@ -20,11 +16,8 @@ class HomeController extends Controller
     public function __construct()
     {
 
-  $s = Controller::connectToApi();
-  $test = $s->get('https://mdapp.kins.ir/cins/rest/anvaGhataatKhodro/findByFilter')->getBody();
-
-$rr =json_decode($test,true);
-print_r($rr[1]['naam']);
+//  $test = Controller::apiGet('rest/takhfifJarimehAyinnameh96/findByFilter');
+//dd($test);
         $this->middleware('auth');
     }
 
